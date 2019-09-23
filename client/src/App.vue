b1fdf5a4c431d0cc6e20c229cb55dd750db1e387
Herbert Lago <herbert.lago@leads2b.com>
2019-09-23 14:51:12 -0300

    Adding buefy library to project
    Signed-off-by: Herbert Lago <herbert.lago@leads2b.com>

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <b-button loading>Click Me</b-button>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
