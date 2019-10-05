e39851e32cc82f9e8a75e25b9cbe41376a7af948
luizdalbello <luiz.dalbello@outlook.com.br>
2019-10-05 12:07:06 -0300

    Adding course crud

--- server/src/database/migrations/20191002234702-create-cursodisciplina.js
+++ server/src/database/migrations/20191002234702-create-cursodisciplina.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('CursoDisciplina', {
+    return queryInterface.createTable('Cursodisciplina', {
       idCurso: {
         type: Sequelize.INTEGER,
         references: {
